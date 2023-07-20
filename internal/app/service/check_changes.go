3f266c776911772491105c3ce37c60922b3055e1
qusysert <is.demchuk@gmail.com>
2023-07-20 10:35:30 +0300

    fix: remove extra logging

--- internal/app/service/check_changes.go
+++ internal/app/service/check_changes.go
@@ -12,10 +12,8 @@ func (s *Service) CheckChanges(ctx context.Context, table string, lastReqTime ti
 		return false, fmt.Errorf("cant get last modified time: %w", err)
 	}
 	if dbLastModifiedTime.After(lastReqTime.Add(time.Second * 60 * 60 * 3)) {
-		fmt.Printf("CheckChanges(): (%v db) is AFTER (req %v)", dbLastModifiedTime.Format("2006-01-02T15:04:05.999999999Z07:00"), s.LastRequestTime().Format("2006-01-02T15:04:05.999999999Z07:00"))
 		return true, nil
 	}
-	fmt.Printf("CheckChanges(): (%v db) is BEFORE (req %v)", dbLastModifiedTime.Format("2006-01-02T15:04:05.999999999Z07:00"), s.LastRequestTime().Format("2006-01-02T15:04:05.999999999Z07:00"))
 
 	return false, nil
 }
