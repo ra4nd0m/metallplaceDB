c88dc78def28de5ed5fb73843b5e57dfa5fe1870
qusysert <is.demchuk@gmail.com>
2023-09-19 17:51:41 +0300

    fix: get_text_width.js

--- docx-gen/src/utils/get_text_width.js
+++ docx-gen/src/utils/get_text_width.js
@@ -14,6 +14,10 @@ module.exports = function getTextWidthInMm(text, fontSize, fontFamily) {
     // Convert the width to millimeters (assuming 96 dpi)
     const widthInMm = textWidth * 25.4 / 96;
 
+    if (text.length <= 12 ) {
+        return widthInMm * 0.8
+    }
+
     // Return the result
-    return widthInMm * 0.76;
+    return widthInMm * 0.73;
 }
