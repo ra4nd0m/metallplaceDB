8aa5d91af4d7781b994d0781189c92d4bc06edb0
qusysert <is.demchuk@gmail.com>
2023-09-29 12:11:13 +0300

    chore: new define_font.js algo

--- docx-gen/src/utils/define_font.js
+++ docx-gen/src/utils/define_font.js
@@ -1,7 +1,20 @@
 const {FontFamilyExtraBold, FontFamily} = require("../const");
-module.exports = function(i, feed, type) {
-    let font = FontFamily
-    if ((i === feed.length - 1 || (i === feed.length - 2 && type !== "month")) && feed.length >= 8) font = FontFamilyExtraBold
-    if (i === 4 && feed.length === 5) font = FontFamilyExtraBold
-    return font
+module.exports = function(i, feed) {
+    if (feed.length >= 2){
+        if (i === feed.length - 1) {
+            return FontFamilyExtraBold
+        }
+        let dateFirst = new Date(feed[feed.length - 2].date)
+        let dateSecond = new Date(feed[feed.length - 1].date)
+        let diffInMilliseconds = Math.abs(dateSecond - dateFirst)
+        let diff = Math.ceil(diffInMilliseconds / (1000 * 60 * 60 * 24));
+        // if updates 2 time a week - two last rows are bold; if once a week - only the last one
+        if (i === feed.length - 2){
+            if (diff < 5) {
+                return FontFamilyExtraBold
+            }
+            return FontFamily
+        }
+    }
+    return FontFamily
 }
