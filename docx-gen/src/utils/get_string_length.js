cf157da94fe709a3d8bafcdecd224286835ddd92
qusysert <is.demchuk@gmail.com>
2023-10-02 07:41:14 +0300

    string width fix

--- docx-gen/src/utils/get_string_length.js
+++ docx-gen/src/utils/get_string_length.js
@@ -1,5 +1,6 @@
 const pixelWidth = require("string-pixel-width");
 module.exports = function (str, fontSize, dpi) {
     const pixelWidthValue = pixelWidth(str, { size: fontSize });
+    if (str.length > 30) return (pixelWidthValue / dpi) * 25.4 * 0.8;
     return (pixelWidthValue / dpi) * 25.4;
 }
