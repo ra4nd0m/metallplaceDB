45d4b6c9da038e879fa8bf480a0b3b066e3b338c
qusysert <is.demchuk@gmail.com>
2024-01-11 14:17:48 +0300

    fix: correct handling pf rounding negative numbers

--- docx-gen/src/utils/get_change.js
+++ docx-gen/src/utils/get_change.js
@@ -1,6 +1,10 @@
 const {ColorDefault, Red, Green} = require("../const");
 const formatNum = require("../utils/numbers_format")
-module.exports = function (feed, i, prevPrice, getPercent, round) {
+function round(v) {
+    return (v >= 0 || -1) * Math.round(Math.abs(v));
+}
+
+module.exports = function (feed, i, prevPrice, getPercent, symbolsAfterDot) {
     let change
    // i === 0 ? change = feed[i].value - prevPrice : change = feed[i].value - feed[i - 1].value
     if (i === 0){
@@ -10,18 +14,18 @@ module.exports = function (feed, i, prevPrice, getPercent, round) {
         prevPrice = feed[i - 1].value
     }
     if (getPercent) {
-        let percent = Math.round(change / prevPrice * 1000) / 10
-        if (percent > 0) return {Text:`+${formatNum(percent, round)}`, Color: Green};
-        if (percent < 0) return {Text:`${formatNum(percent, round)}`, Color: Red};
+        let percent = round(change / prevPrice * 1000) / 10
+        if (percent > 0) return {Text:`+${formatNum(percent, symbolsAfterDot)}`, Color: Green};
+        if (percent < 0) return {Text:`${formatNum(percent, symbolsAfterDot)}`, Color: Red};
         return {Text:"-", Color: ColorDefault};
     } else {
-        change = Math.round(change * 100) / 100
-        if (round !== undefined){
-            const n =  Math.pow(10, round)
-            change = Math.round(change * n) / n
+        change = round(change * 100) / 100
+        if (symbolsAfterDot !== undefined){
+            const n =  Math.pow(10, symbolsAfterDot)
+            change = round(change * n) / n
         }
-        if (change > 0) return {Text:`+${formatNum(change, round)}`, Color: Green};
-        if (change < 0) return {Text:`${formatNum(change, round)}`, Color: Red};
+        if (change > 0) return {Text:`+${formatNum(change, symbolsAfterDot)}`, Color: Green};
+        if (change < 0) return {Text:`${formatNum(change, symbolsAfterDot)}`, Color: Red};
         return {Text:"-", Color: ColorDefault};
     }
 }
